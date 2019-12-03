d573fe20b744537e793019d351602909f073224c
Tryggvi Ólason <tryggvi14@ru.is>
2019-12-03 14:28:11 +0000

    contact detail

---
+++ src/components/ContactDetail/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    //flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fc5203'
+    //justifyContent: 'center',
+  },
+  image: {
+    marginTop: 50,
+    //alignSelf: 'center',
+  },
+  info: {
+    fontSize: 15,
+    paddingLeft: 10,
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+});
