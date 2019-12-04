10625fe4f88cf9eaf9a030fac3ba26666bfb0e66
Arnarkj <arnark18@ru.is>
2019-12-04 05:46:23 +0000

    Added ability to make phone calls

--- src/components/ContactDetail/index.jsx
+++ src/components/ContactDetail/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { View, Text, Image, TouchableHighlight } from 'react-native';
+import {
+  View, Text, Image, Button
+} from 'react-native';
+import { Linking } from 'expo';
 import styles from './styles'
 
 
 export default class ContactDetail extends React.Component {
   render() {
     const { navigation } = this.props;
+    const phoneNumber = navigation.getParam('contactPhoneNumber', 'default value');
     return (
       <View style={styles.container}>
         <Image source={{ uri: navigation.getParam('contactPhoto', 'default value') }} style={styles.image}/>
         <Text style={styles.header}>Name</Text>
         <Text style={styles.info}> {navigation.getParam('contactName', 'default value')}</Text>
         <Text style={styles.header}>Phonenumber </Text>
-        <Text style={styles.info}> {navigation.getParam('contactPhoneNumber', 'default value')} </Text>
+        <Button
+          style={styles.info}
+          title={phoneNumber}
+          onPress={() => { Linking.openURL(`tel:${phoneNumber}`); }}
+        />
       </View>
     );
   }
