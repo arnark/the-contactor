1f3339f53b4bcb50d545bdbce99c53a05563a65d
Bjarki Baldvinsson <bjarkib16@ru.is>
2019-12-05 11:07:15 +0000

    editcontact continued, not finished

--- src/views/EditContact/index.jsx
+++ src/views/EditContact/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import EditContactInput from '../../components/EditContactInput';
 
 export default class NewContact extends React.Component {
-
   render() {
     const { navigate } = this.props.navigation;
     console.log(this.props.navigation)
@@ -10,10 +9,6 @@ export default class NewContact extends React.Component {
       <>
         <EditContactInput
           navigation={this.props.navigation}
-          contactName={this.props.navigation.state.params.contactName}
-          contactId={this.props.navigation.state.params.contactId}
-          contactPhoto={this.props.navigation.state.params.contactPhoto}
-          contactPhoneNumber={this.props.navigation.state.params.contactPhoneNumber}
         />
       </>
     );
